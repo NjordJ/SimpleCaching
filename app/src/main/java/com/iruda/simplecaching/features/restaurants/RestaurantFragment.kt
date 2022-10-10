9b2db957092f16842f365f2fc9cfe7c6cd1792da
NjordJ <nj0rd3841@gmail.com>
2022-10-10 16:00:41 +0500

    Layouts

--- app/src/main/java/com/iruda/simplecaching/features/restaurants/RestaurantFragment.kt
+++ app/src/main/java/com/iruda/simplecaching/features/restaurants/RestaurantFragment.kt
@@ -1,6 +1,38 @@
 package com.iruda.simplecaching.features.restaurants
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.iruda.simplecaching.databinding.FragmentRestaurantsBinding
 
 class RestaurantFragment : Fragment() {
+
+    private var _binding: FragmentRestaurantsBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRestaurantsBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentRestaurantsBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
